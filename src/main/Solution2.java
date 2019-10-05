fe20801fd661b4477317138437c450ca082d19e2
Nitin Faujdar <nitin.faujdar_iot18@gla.ac.in>
2019-10-05 22:35:07 +0530

    Create Solution2.java

---
+++ src/main/Solution2.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class Solution2 {
+
+    public static void main(String[] args) {
+              Scanner sc = new Scanner(System.in);
+              String s = sc.nextLine();
+              int i =sc.nextInt();
+              int j =sc.nextInt();
+              System.out.println(s);
+              System.out.println(s.substring(i,j));
+
+    }
+
+}
